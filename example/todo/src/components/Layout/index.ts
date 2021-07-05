cf88d6f607a28075f9fefccc1d8ca191ed63c9d8
parkeunsik <qkrdmstlr3@naver.com>
2021-07-05 21:11:36 +0900

    [feat] example/todo > develop change introduce text event
    - In Layout component, develop change introduce event
      - to show how to control local state

--- example/todo/src/components/Layout/index.ts
+++ example/todo/src/components/Layout/index.ts
@@ -4,8 +4,13 @@ import {
   useGlobalState,
 } from "../../lib/shell-html";
 import StyleSheet from "./style.scss";
+import { EventType } from "../../lib/shell-html/type";
 
 class Layout extends ShellHTML {
+  constructor() {
+    super("developer");
+  }
+
   connectedCallback() {
     this.enrollObserving("page");
   }
@@ -14,12 +19,23 @@ class Layout extends ShellHTML {
     this.releaseObserving("page");
   }
 
+  changeIntroduceHandler() {
+    if (this.state === "developer") {
+      this.setState("welcome");
+      return;
+    }
+    this.setState("developer");
+  }
+
   render() {
     const page = useGlobalState("page");
+    const introduce =
+      this.state === "developer" ? "made by shellboy" : "welcome!";
 
     return {
       html: `
       <div class="layout">
+        <span>${introduce} <span class="layout__click">click!</span></span>
         <header-component id="header"></header-component>
         ${
           page === "home"
@@ -28,6 +44,13 @@ class Layout extends ShellHTML {
         }
       </div>`,
       css: StyleSheet,
+      eventFuncs: [
+        {
+          className: "layout__click",
+          func: this.changeIntroduceHandler,
+          type: EventType.click,
+        },
+      ],
     };
   }
 }
