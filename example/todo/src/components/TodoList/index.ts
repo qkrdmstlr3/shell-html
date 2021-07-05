135e3c3687e18c5dc613dac2753b60bb56495ac6
parkeunsik <qkrdmstlr3@naver.com>
2021-07-05 20:40:39 +0900

    [feat] example/todo > develop todolist component
    - get todolist from global state
      - rendering
    - add link to home page

--- example/todo/src/components/TodoList/index.ts
+++ example/todo/src/components/TodoList/index.ts
@@ -1,14 +1,53 @@
-import { createComponent, ShellHTML } from "../../lib/shell-html";
+import {
+  createComponent,
+  ShellHTML,
+  useGlobalState,
+  setGlobalState,
+  EventType,
+} from "../../lib/shell-html";
 import StyleSheet from "./style.scss";
 
+interface TodoItem {
+  id: number;
+  content: string;
+}
+
 class TodoList extends ShellHTML {
+  movePageToHomeHandler() {
+    setGlobalState("page", "home");
+  }
+
+  getTodoListHTML() {
+    const list: TodoItem[] = useGlobalState("todolist");
+    return list.reduce(
+      (acc, item) =>
+        (acc += `
+        <li class="todolist__item">
+          <p>${item.id}. ${item.content}</p>
+          <button id="${item.id}">delete</button>
+        </li>
+        `),
+      ""
+    );
+  }
+
   render() {
+    const todoListHTML = this.getTodoListHTML();
+
     return {
       html: `
       <div class="todolist">
-        todolist
+        <a class="todolist__link">back to home</a>
+        <ul class="todolist__list">${todoListHTML}</ul>
       </div>`,
       css: StyleSheet,
+      eventFuncs: [
+        {
+          className: "todolist__link",
+          func: this.movePageToHomeHandler,
+          type: EventType.click,
+        },
+      ],
     };
   }
 }
