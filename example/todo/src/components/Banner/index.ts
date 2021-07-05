3464ee8177441ca83540984b9338a165e3a1236a
parkeunsik <qkrdmstlr3@naver.com>
2021-07-05 19:54:32 +0900

    [feat] example/todo > develop banner, change page
    - develop banner
      - can enroll input item
      - can move todlist page
    - layout enroll global page state

--- example/todo/src/components/Banner/index.ts
+++ example/todo/src/components/Banner/index.ts
@@ -1,14 +1,34 @@
-import { createComponent, ShellHTML } from "../../lib/shell-html";
+import {
+  createComponent,
+  ShellHTML,
+  setGlobalState,
+  EventType,
+} from "../../lib/shell-html";
 import StyleSheet from "./style.scss";
 
 class Banner extends ShellHTML {
+  movePageToListHandler() {
+    setGlobalState("page", "todolist");
+  }
+
   render() {
     return {
       html: `
-      <div class="banner">
-        banner
-      </div>`,
+      <main class="banner">
+        <div class="banner__todobox">
+          <input placeholder="input todo item" class="banner__todobox__input" />
+          <button class="banner__todobox__button">enroll</button>
+        </div>
+        <a class="banner__link">move to list</a>
+      </main>`,
       css: StyleSheet,
+      eventFuncs: [
+        {
+          className: "banner__link",
+          func: this.movePageToListHandler,
+          type: EventType.click,
+        },
+      ],
     };
   }
 }
--- example/todo/src/components/Layout/index.ts
+++ example/todo/src/components/Layout/index.ts
@@ -1,13 +1,31 @@
-import { createComponent, ShellHTML } from "../../lib/shell-html";
+import {
+  createComponent,
+  ShellHTML,
+  useGlobalState,
+} from "../../lib/shell-html";
 import StyleSheet from "./style.scss";
 
 class Layout extends ShellHTML {
+  connectedCallback() {
+    this.enrollObserving("page");
+  }
+
+  disconnectedCallback() {
+    this.releaseObserving("page");
+  }
+
   render() {
+    const page = useGlobalState("page");
+
     return {
       html: `
       <div class="layout">
         <header-component id="header"></header-component>
-        <todolist-component id="todo"></todolist-component>
+        ${
+          page === "home"
+            ? '<banner-component id="banner"></banner-component>'
+            : '<todolist-component id="todo"></todolist-component>'
+        }
       </div>`,
       css: StyleSheet,
     };
