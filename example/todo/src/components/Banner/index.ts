ee0262afd37b2a6d42d610e4ba72e43147bde9a1
parkeunsik <qkrdmstlr3@naver.com>
2021-07-05 21:02:30 +0900

    [feat] example/todo > develop TodoItem create event
    - In Banner component, develop todoItem create event

--- example/todo/src/components/Banner/index.ts
+++ example/todo/src/components/Banner/index.ts
@@ -1,24 +1,46 @@
 import {
   createComponent,
   ShellHTML,
+  useGlobalState,
   setGlobalState,
   EventType,
 } from "../../lib/shell-html";
 import StyleSheet from "./style.scss";
+
+interface TodoItem {
+  id: number;
+  content: string;
+}
 
 class Banner extends ShellHTML {
   movePageToListHandler() {
     setGlobalState("page", "todolist");
   }
 
+  createNewTodoItemHandler(event: Event) {
+    event.preventDefault();
+
+    const list: TodoItem[] = useGlobalState("todolist");
+    const input = this.querySelector("input") as HTMLInputElement;
+
+    setGlobalState("todolist", [
+      ...list,
+      {
+        id: list.length ? list[list.length - 1].id + 1 : 1,
+        content: input.value,
+      },
+    ]);
+    input.value = "";
+  }
+
   render() {
     return {
       html: `
       <main class="banner">
-        <div class="banner__todobox">
+        <form class="banner__todobox">
           <input placeholder="input todo item" class="banner__todobox__input" />
           <button class="banner__todobox__button">enroll</button>
-        </div>
+        </form>
         <a class="banner__link">move to list</a>
       </main>`,
       css: StyleSheet,
@@ -27,6 +49,11 @@ class Banner extends ShellHTML {
           className: "banner__link",
           func: this.movePageToListHandler,
           type: EventType.click,
+        },
+        {
+          className: "banner__todobox",
+          func: this.createNewTodoItemHandler,
+          type: EventType.submit,
         },
       ],
     };
